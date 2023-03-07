668b7e2993f98f44e26a9cfba7f0402596c8c6e5
crispynoodlesoup <thepanman321@gmail.com>
2023-03-06 20:45:42 -0600

    write template functions for Game class

--- app.js
+++ app.js
@@ -2,14 +2,32 @@ const Game = (() => {
   playerTurn = "blue";
   let boardArray;
   [...boardArray] = Array.from(document.querySelectorAll(".square div"));
+  const winningPositions = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
 
-  // for troubleshooting
+  // for troubleshooting the board
   const print = function () {
     console.log(...boardArray);
+    console.log(checkWin());
   };
+
+  const makeMove = function (index) {};
+
+  const checkWin = function () {};
+
+  const displayWin = function () {};
 
   return {
     print,
+    makeMove,
   };
 })();
 
